0cb13b662c0ba6762ee77b25153ae790a9162c9d
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2026-03-12 13:28:22 +0300

    fix: remove basePath and trailing slash from all environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,10 +2,9 @@ import { endpoints } from '../app/config/endpoints';
 
 export const environment = {
   production: false,
-  apiUrl: `https://syncboard-ptvu.onrender.com/`,
+  apiUrl: 'https://syncboard-ptvu.onrender.com',
   api: {
-    basePath: 'api/v2.0',
+    basePath: '',
     endpoints: endpoints
   }
 };
-
