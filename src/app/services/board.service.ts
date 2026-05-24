1cd4d0b4db43c68e233004b04096159805ba1ff5
jepkemoi-sheilah1 <jepkemoi-sheilah1@users.noreply.github.com>
2026-05-24 20:33:07 +0300

    fix: correct board endpoints for get all and create

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { Board, CreateBoardRequest, UpdateBoardRequest } from '../models/board.models';
 
@@ -13,19 +14,32 @@ export class BoardService {
   constructor(private http: HttpClient) {}
 
   getBoard(boardId: string): Observable<Board> {
-    return this.http.get<Board>(`${this.base}/boards/${boardId}`);
+    return this.http.get<any>(`${this.base}/boards/${boardId}`).pipe(
+      map(response => response.data ?? response)
+    );
   }
 
   getBoardsByWorkspace(workspaceId: string): Observable<Board[]> {
-    return this.http.get<Board[]>(`${this.base}/workspaces/${workspaceId}/boards`);
+    return this.http.get<any>(`${this.base}/boards`).pipe(
+      map(response => response.data ?? response)
+    );
   }
 
   createBoard(request: CreateBoardRequest): Observable<Board> {
-    return this.http.post<Board>(`${this.base}/boards`, request);
+    return this.http.post<any>(
+      `${this.base}/boards/${request.workSpaceId}`,
+      {
+        boardName: request.boardName,
+        boardDescription: request.boardDescription,
+        isStarred: request.isStarred ?? false
+      }
+    ).pipe(map(response => response.data ?? response));
   }
 
   updateBoard(boardId: string, request: UpdateBoardRequest): Observable<Board> {
-    return this.http.put<Board>(`${this.base}/boards/${boardId}`, request);
+    return this.http.put<any>(`${this.base}/boards/${boardId}`, request).pipe(
+      map(response => response.data ?? response)
+    );
   }
 
   deleteBoard(boardId: string): Observable<void> {
